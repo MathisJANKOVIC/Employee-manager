07fbde98efb3b54f464e808a73ed9f3e62f10c40
unknown <mathis.jankovic@gmail.com>
2023-12-20 18:25:53 +0100

    add database setup

--- src/main/java/com/example/employeemanager/Home.java
+++ src/main/java/com/example/employeemanager/Home.java
@@ -4,9 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Hello {
+public class Home {
+
     @GetMapping("/")
-    public String hello() {
-        return "Employee Manager";
+    public String home() {
+        return "Welcome to Employee Manager";
     }
 }
